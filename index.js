9d667b805ec3e373433ceac91626fed63572b142
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-04 13:16:55 +0100

    .\index.html

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Read instructions. And remove this line.
+// Read instructions. And remove this line.f
+
+function randomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+const secretNumber = randomInt(100)
+
+function getUserGuess () {
+    return 14
+}
+
+globalThis.getUserGuess = getUserGuess
