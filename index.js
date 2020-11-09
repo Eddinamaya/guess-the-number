1ca9b98962abf306908417c9e595f09f929b6a0d
Eddin Amaya <badreddin.amaya@skola.botkyrka.se>
2020-11-09 14:18:10 +0100

    .\index.js

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ function randomInt(max) {
 }
 const secretNumber = randomInt(100)
 
-function getUserGuess () {
-    return 14
-}
+// function getUserGuess () {
+//     return 14
+// }
 
 globalThis.getUserGuess = getUserGuess
+
+// uppgift 2
+
+function getUserGuess(){
+    const guess = document.getElementById('user-input').value
+    return parseInt(guess, 10) 
+        
+    }
+
+    // uppgift 3
+
+    document.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            const guess = getUserGuess()
+            console.log(guess)
+        }
+    })
+
+
